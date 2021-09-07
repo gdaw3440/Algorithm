7e273131a6db3f6d257c843f0de64d0c7070f8e3
Zephyr <gdaw3440@naver.com>
2021-09-07 22:13:34 +0900

    Chapter 02 대소문자 변환

---
+++ src/Algorithm/Chapter02.java
@@ -0,0 +1,23 @@
+package Algorithm;
+
+import java.util.Scanner;
+
+public class Chapter02 {
+    public String solution(String str) {
+        String answer = "";
+        for (char x : str.toCharArray()) {
+            if(Character.isLowerCase(x)) {
+                answer+= Character.toUpperCase(x);
+            } else {
+                answer+= Character.toLowerCase(x);
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args){
+        Chapter02 chapter02 = new Chapter02();
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        System.out.println(chapter02.solution(str));
+    }
+}
