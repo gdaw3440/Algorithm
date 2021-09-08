a9dbd7e035447ac48df1644bb14097ab5ef87991
gdaw3440 <gdaw3440@naver.com>
2021-09-08 15:02:25 +0900

    Chapter 07. 회문 문자열(스트링빌더 추가)

--- src/Algorithm/String/Chapter07.java
+++ src/Algorithm/String/Chapter07.java
@@ -10,6 +10,8 @@ public class Chapter07 { // Main
         for(int i=0; i<len/2; i++) {
             if(str.charAt(i)!=str.charAt(len-i-1)) return "NO";
         }
+        //String answer = "NO";
+        //if (str.equalsIgnoreCase(new StringBuilder(str).reverse().toString())) answer = "YES";
         return answer;
     }
     public static void main(String[] args){
