5b0736373e9122493ddaa5ed4f0aebafa97d2600
gdaw3440 <gdaw3440@naver.com>
2021-09-08 14:59:00 +0900

    Chapter 07. 회문 문자열

--- src/Algorithm/String/Chapter07.java
+++ src/Algorithm/String/Chapter07.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Chapter07 { // Main
     public String solution(String str) {
         String answer = "YES";
+        str = str.toUpperCase();
         int len = str.length();
         for(int i=0; i<len/2; i++) {
             if(str.charAt(i)!=str.charAt(len-i-1)) return "NO";
